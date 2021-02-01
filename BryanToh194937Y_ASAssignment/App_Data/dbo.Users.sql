41fe142c2e425fe462dfd9c376e92e5f24d2ac67
Bryan Toh <warmbryan@gmail.com>
2021-02-01 20:13:31 +0800

    sql tables creation files for new databases

---
+++ BryanToh194937Y_ASAssignment/App_Data/dbo.Users.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [FirstName]    NVARCHAR (30)    NOT NULL,
+    [LastName]     NVARCHAR (30)    NOT NULL,
+    [CCNo]         NVARCHAR (MAX)   NOT NULL,
+    [CCExpiry]     NVARCHAR (MAX)   NOT NULL,
+    [CCCVV]        NVARCHAR (MAX)   NOT NULL,
+    [Email]        NVARCHAR (320)   NOT NULL,
+    [PasswordHash] NVARCHAR (MAX)   NOT NULL,
+    [PasswordSalt] NVARCHAR (MAX)   NOT NULL,
+    [DateOfBirth]  DATE             NOT NULL,
+    [IV]           NVARCHAR (MAX)   NOT NULL,
+    [Key]          NVARCHAR (MAX)   NOT NULL,
+    [RegDate]      DATETIME         DEFAULT (getdate()) NOT NULL,
+    [LastPassDate] DATETIME         DEFAULT (getdate()) NOT NULL,
+    [FailedLogin]  INT              DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
