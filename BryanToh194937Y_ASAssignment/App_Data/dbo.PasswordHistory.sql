cb741697c304dbfdb9e54505a9a71f1f5d3ee4dc
Bryan Toh <warmbryan@gmail.com>
2021-02-01 21:19:00 +0800

    Revert "sql tables creation files for new databases"
    This reverts commit 41fe142c2e425fe462dfd9c376e92e5f24d2ac67.

--- BryanToh194937Y_ASAssignment/App_Data/dbo.PasswordHistory.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PasswordHistory] (
-    [Id]        UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
-    [Hash]      VARCHAR (MAX)    NOT NULL,
-    [UserId]    UNIQUEIDENTIFIER NOT NULL,
-    [CreatedOn] DATETIME         DEFAULT (getdate()) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [cPHUserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
-);
-
